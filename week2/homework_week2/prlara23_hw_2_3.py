f15947df10d976af1151d41f878dabebce020f0b
ramirez <prlara2000@gmail.com>
2023-01-25 23:00:22 -0600

    add homework 2.3

---
+++ week2/homework_week2/prlara23_hw_2_3.py
@@ -0,0 +1,12 @@
+'''
+Pedro Ramirez
+homework 2.3
+CS-521
+prints formula and result of user's input
+'''
+n = int(input('Enter a number: '))
+
+result = n**3 / n
+
+# prints the formula and result
+print(f'{n}**3/{n} = {result:.2f}')
