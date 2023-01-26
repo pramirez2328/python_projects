5b2581a5008a2392c617911ba13e0286d757cdf2
ramirez <prlara2000@gmail.com>
2023-01-25 23:10:29 -0600

    add homework 2.4

---
+++ week2/homework_week2/prlara23_hw_2_4.py
@@ -0,0 +1,12 @@
+'''
+Pedro Ramirez
+homework 2.4
+CS-521
+prints remainder
+'''
+
+number = input('Enter a number: ')
+number = int(number)
+
+# prints 0 if the number is even, 1 if the number is odd
+print(number % 2)
