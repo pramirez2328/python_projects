09bd31bc7c79e21a9a8c824eaa74866c8a02b231
ramirez <prlara2000@gmail.com>
2023-01-27 10:28:20 -0600

    add practice exercises

---
+++ week2/practice.py
@@ -0,0 +1,13 @@
+# m = 2
+# for n in range(3, 15, 5):
+#     n += m + 2
+# print(n)
+
+# x_list = []
+# for n in range(4):
+#     x_list.append(n**2)
+# print(x_list)
+
+x = {"salmon": "fish", "orca": "whale"}
+y = x["fish"]
+print(y)
