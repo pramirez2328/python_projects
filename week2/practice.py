31bce43692fbc173091c946e2a95de88e5e860bb
ramirez <prlara2000@gmail.com>
2023-01-30 20:37:54 +0100

    add examples

--- week2/practice.py
+++ week2/practice.py
@@ -8,6 +8,15 @@
 #     x_list.append(n**2)
 # print(x_list)
 
-x = {"salmon": "fish", "orca": "whale"}
-y = x["fish"]
-print(y)
+# x = {"salmon": "fish", "orca": "whale"}
+# y = x["fish"]
+# print(y)
+
+# print("Hello World \nthis is college \t and this is a tab")
+# print('Hello World \nthis is college \t and this is a tab')
+
+example = "Hello World"
+
+print(example[0:5])
+print(example[6:])
+print(example[:5:2])
